59d9ec902eb8681666cf5a2461ba817fc7af4123
Juan Forcada <154464325+juanforcada@users.noreply.github.com>
2025-03-27 20:33:23 -0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// Función saludo usando template strings
-function saludo(nombre, mensaje) {
-  console.log(`${mensaje}, ${nombre}!`);
-  }
-  
-  // Ejemplos de uso:
-  saludo("Rosario", "¡Hola"); // Resultado: ¡Hola, Rosario!
-  saludo("Pepe", "¡Buen trabajo"); // Resultado: ¡Buen trabajo, Pepe!
